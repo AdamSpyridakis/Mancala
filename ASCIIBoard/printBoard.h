d57841e41172adc46fb14b15584dbb80ddca9e56
unknown <ajspyridakis@gmail.com>
2024-07-20 18:11:02 -0400

    updated start game function

--- ASCIIBoard/printBoard.h
+++ ASCIIBoard/printBoard.h
@@ -7,5 +7,6 @@
 // All terminal prints/inputs are declared here. Will make it easier to convert to non-terminal eventually
 void printBoard(board* ptr);
 int getMove(bool player, board* ptr);
+void checkWinner(board* ptr);
 
 #endif
